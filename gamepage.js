928ca3ff537d0b804d84257ca1f31e935bde6f4a
ImonPanda <153349236+ImonPanda@users.noreply.github.com>
2024-05-15 15:04:12 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,31 @@
+// generation of random value
+var y = Math.floor(Math.random() * 10 + 1);
+
+var x = document.getElementById("guessField").value
+
+if(x == y)
+    {
+        alert(" CONGRADULATION! " +playername+ " YOU GUESSED IT RIGHT IN " +guess+ "GUESS");
+        guess = 1;
+    }
+
+    else if(x > y)
+        {
+            guess++;
+            alert("OOPS SORRY! THIS IS A WRONG GUESS, TRY A SMALLER NUMBER")
+        }
+        else
+        {
+            guess++;
+            alert("OOPS SORRY! THIS IS A WRONG GUESS, TRY A GREATER NUMBER")
+        }
+
+        function playAgain()
+        {
+           y = Math.floor(Math.random() * 10 + 1);
+        }
+    
+    // count of attempts
+// until hit
+  
+// function for the number sent by the user
